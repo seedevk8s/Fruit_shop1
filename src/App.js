211a874e1a9467bf45d33dfde561faa4cd9c74be
seedevk8s <seedevk8s@gmail.com>
2025-08-03 10:32:23 +0900

    상세페이지 하단 sub 탭 UI 만들기

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import Products from "./components/Products";
-import { Button, Navbar, Container, Nav } from "react-bootstrap";
-import data from "./db/fruit";
-import { useState } from "react";
-import Detail from "./components/Detail";
-import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
-import Title from "./components/Title";
+import './App.css';
+import {Button, Navbar, Container, Nav} from 'react-bootstrap'
+import data from './db/fruit';
+import { useState } from 'react';
+import Products from './components/Products';
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom'
+import Detail from './components/Detail';
+import Title from './components/Title'
+import Title2 from './components/Title2'
+import data2 from "./db/veggie";
+import ComVeggie from "./components/ComVeggie";
+import Footer from './components/Footer'
+import axios from 'axios'
 
 function App() {
   let [fruit, setFruit] = useState(data);
-  let navigate = useNavigate();
+ // console.log(fruit[0].price);
+ let [veggie, setVeggie] = useState(data2);
+ let [count, setCount] = useState(1);
 
   const sortByName = () => {
     let sortedFruit = [...fruit].sort((a, b) => (a.title > b.title ? 1 : -1));
@@ -28,121 +36,141 @@ function App() {
     console.log(sortedFruit);
   };
 
+  let navigate = useNavigate();
+
   return (
-    <div className="App" style={{ textAlign: "center" }}>
+    <div className="App">
+
       <>
         <Navbar bg="dark" variant="dark">
           <Container>
-            <Navbar.Brand href="#home">과일농장</Navbar.Brand>
+            <Navbar.Brand onClick={()=>{ navigate('/')}}>과일농장</Navbar.Brand>
             <Nav className="me-auto">
-              <Nav.Link
-                onClick={() => {
-                  navigate("/");
-                }}
-              >
-                홈으로
-              </Nav.Link>
-              <Nav.Link
-                onClick={() => {
-                  navigate("/detail");
-                }}
-              >
-                상세페이지
-              </Nav.Link>
-              <Nav.Link
-                onClick={() => {
-                  navigate("/cart");
-                }}
-              >
-                장바구니
-              </Nav.Link>
-              <Nav.Link
-                onClick={() => {
-                  navigate("/about");
-                }}
-              >
-                회사소개
-              </Nav.Link>
+                <Nav.Link onClick={()=>{ navigate('/')}}>홈으로</Nav.Link>
+                <Nav.Link onClick={()=>{ navigate('/detail/1')}}>상세페이지</Nav.Link>
+                <Nav.Link onClick={() => { navigate('/cart') }}>장바구니</Nav.Link> 
+                <Nav.Link onClick={() => { navigate('/about') }}>회사소개</Nav.Link> 
             </Nav>
           </Container>
         </Navbar>
 
+
+        {/* <Link to="/">홈</Link><br></br>
+        <Link to="/detail">상세페이지</Link>  */}
+
+        
         <Routes>
-          <Route
-            path="/"
-            element={
-              <div>
-                <div className="slider"></div>
-                <Title />
+          <Route path="/" element={<div>
 
-                <div class="container">
-                  <div class="row">
-                    <div style={{ textAlign: "center" }}>
-                      <Button variant="outline-primary" onClick={sortByName}>
-                        {" "}
-                        이름순 정렬{" "}
-                      </Button>{" "}
-                      <Button
-                        variant="outline-secondary"
-                        onClick={sortByPriceLowToHigh}
-                      >
-                        낮은가격순 정렬
-                      </Button>{" "}
-                      <Button
-                        variant="outline-success"
-                        onClick={sortByPriceHighToLow}
-                      >
-                        높은가격순 정렬
-                      </Button>{" "}
-                    </div>
+            <div className="slider"></div>
+
+            <Title/>
+
+            <div class="container">
+                <div class="row">
+                      <div style={{ textAlign: "center" }}>
+                            <Button variant="outline-primary" onClick={sortByName}> 이름순 정렬 </Button>{" "}
+                            <Button variant="outline-secondary" onClick={sortByPriceLowToHigh}>낮은가격순 정렬</Button>{" "}
+                            <Button variant="outline-success" onClick={sortByPriceHighToLow}>높은가격순 정렬</Button>{" "}
+                      </div>
                   </div>
+            </div>
+
+              <div className="container" style={{textAlign:"center", marginTop:"30px"}}>
+                <div className="row">
+                          
+                  {
+                      fruit.map((ele,i)=>{
+                        return(
+                          <Products fruit={fruit[i]} i={i}  key={fruit[i].id} />
+                        )
+                      })
+                  }
+
                 </div>
 
-                <div className="container" style={{ marginTop: "30px" }}>
-                  <div className="row">
-                    {fruit.map((ele, i) => {
-                      return (
-                        <Products fruit={fruit[i]} i={i} key={fruit[i].id} />
-                      );
-                    })}
-                  </div>
-                </div>
+              </div> 
+
+              
+              <Button variant="outline-success" count = {count} onClick={() => {
+                 
+                 if(count==1){
+                   axios.get('https://dino-21.github.io/react_data/veggie2.json').then((result)=>{
+                       let copy10 =[...veggie, ...result.data];
+                       setVeggie(copy10);
+                       setCount(count + 1);
+              
+                 })}else if(count==2){
+                   axios.get('https://dino-21.github.io/react_data/veggie3.json').then((result)=>{
+                     let copy11 =[...veggie, ...result.data];
+                     setVeggie(copy11);
+                     setCount(count + 1);
+                    })   
+                 }
+      
+                 if(count===3){
+                   alert("더이상 상품이 없습니다.");  
+                 }
+              }}> + 3개 상품 더 보기 </Button>{' '}   
+
+
+              <div className="container" style={{ marginTop: "30px" }}>
+                            <div className="row">
+                                  {veggie.map((ele, i) => {
+                                        return <ComVeggie veggie={veggie[i]} key={veggie[i].id} />;
+                                      })}
+                            </div>
               </div>
-            }
-          />
-          <Route path="detail/:paramId" element={<Detail fruit={fruit} />} />
-          <Route path="/about" element={<About />}>
-            <Route path="member" element={<Member />} />
-            <Route path="location" element={<Location />} />
+
+              <Footer/>
+
+          </div>} />
+
+
+          <Route path="/detail/:paramId" element={<Detail fruit={fruit}/>} />
+
+          <Route path="/about" element={<About/>} >
+            <Route path="member" element={<Member/>} />
+            <Route path="location" element={<Location/>} />
           </Route>
         </Routes>
+        
+   
       </>
+
     </div>
   );
 }
+
+
+//app 아래, export default App; 위에
+function About(){
+  return(
+   <>
+     <h4>회사정보</h4>
+     <Outlet></Outlet>
+   </>
+  );
+}
+
+
+function Member(){
+  return(
+   <>
+     <h4>Member</h4>
+   </>
+  );
+}
+
+
+function Location(){
+  return(
+   <>
+     <h4>Location</h4>
+   </>
+  );
+}
+
+
+
 export default App;
-
-function About() {
-  return (
-    <>
-      <h4>회사정보</h4>
-      <Outlet></Outlet>
-    </>
-  );
-}
-
-function Member() {
-  return (
-    <>
-      <h4>Member</h4>
-    </>
-  );
-}
-
-function Location() {
-  return (
-    <>
-      <h4>Location</h4>
-    </>
-  );
-}
--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useParams } from "react-router-dom";
+import { Nav } from 'react-bootstrap'
+import { useState } from 'react';
 
 function Detail(props) {
     let {paramId} = useParams();
@@ -8,6 +10,7 @@ function Detail(props) {
     // props.fruit 배열에서 id가 paramId와 일치하는 상품을 찾아 item 변수에 저장
     // paramId는 문자열이므로 parseInt를 사용해 숫자로 변환
     let item = props.fruit.find(f => f.id === parseInt(paramId));
+    let [tap, setTap] = useState(0);
 
     const { imgUrl, title, content, price } = item;
     return (
@@ -23,8 +26,27 @@ function Detail(props) {
                     <button className="btn btn-danger">주문하기</button>
                 </div>
             </div>
+
+            <Nav variant="tabs"  defaultActiveKey="link0" style={{marginTop:"50px"}}>
+                <Nav.Item>
+                <Nav.Link  onClick={()=>{ setTap(0) }} eventKey="link0">버튼0</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                <Nav.Link onClick={()=>{ setTap(1) }} eventKey="link1">버튼1</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                <Nav.Link  onClick={()=>{ setTap(2) }} eventKey="link2">버튼2</Nav.Link>
+                </Nav.Item>
+            </Nav>
+
+            <TabContent tap={tap}/>
+
         </div>
     )
+}
+
+function TabContent({tap}){
+    return [ <div>내용0</div>, <div>내용1</div>, <div>내용2</div> ][tap]
 }
 
 export default Detail;
